210689e47076f12a10215cadbfd980a4565dc827
Michael Pautov <michael.pautov@gmail.com>
2019-09-01 10:58:46 +0600

    feat(344): Solved reverse string

---
+++ easy/344-reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  for (let i = 0; i < s.length / 2; i++) {
+    let temp = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+  }
+};
+
+let a = ["h","e","l","l","o"];
+console.log(reverseString(a));
+console.log(a);
+
