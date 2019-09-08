312f35e7ed488c6a99a74dbc5547de464bc794fa
Michael Pautov <michael.pautov@gmail.com>
2019-09-08 09:44:00 +0600

    feat(189): Solved rotate array

---
+++ easy/189-rotate-arrray.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  for (let i = 0; i < k; i++) {
+    const t = nums.pop();
+    nums.unshift(t);
+  }
+  return nums;
+};
+
+// Warn if overriding existing method
+if(Array.prototype.equals)
+  console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+  // if the other array is a falsy value, return
+  if (!array)
+    return false;
+
+  // compare lengths - can save a lot of time
+  if (this.length != array.length)
+    return false;
+
+  for (var i = 0, l=this.length; i < l; i++) {
+    // Check if we have nested arrays
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!this[i].equals(array[i]))
+        return false;
+    }
+    else if (this[i] != array[i]) {
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false;
+    }
+  }
+  return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+console.log(rotate([1,2,3,4,5,6,7], 3).equals([5,6,7,1,2,3,4]));
+console.log(rotate([-1,-100,3,99], 2).equals([3,99,-1,-100]));
