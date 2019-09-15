c38fe69c3e9de20d242cc96fa72107d2097f0d33
Michael Pautov <michael.pautov@gmail.com>
2019-09-15 10:38:53 +0600

    feat(202): Solved happy number

---
+++ easy/202-happy-number.js
@@ -0,0 +1,26 @@
+const squares = {'0':0, '1':1, '2':4, '3':9, '4':16, '5':25, '6':36, '7':49, '8':64, '9':81}
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  if (n < 1) return false;
+
+  let set = new Set();
+
+  while (!set.has(n)) {
+    set.add(n);
+    let s = n.toString();
+    n = 0;
+    for (let i = 0; i < s.length; ++i) {
+      n += squares[s[i]];
+    }
+    if (n == 1) return true;
+  }
+
+  return false;
+};
+
+console.log(isHappy(19));
+console.log(isHappy(1111111));
