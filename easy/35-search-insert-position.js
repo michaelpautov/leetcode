72450d2db8de2e6a7ac8636b303f6359657aeb25
Michael Pautov <michael.pautov@gmail.com>
2019-11-11 09:49:18 +0600

    feat(35): solved

---
+++ easy/35-search-insert-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let l = 0; r = nums.length - 1;
+  while(l <= r) {
+    let m = Math.floor((l + r)/2);
+    if (nums[m] === target) return m;
+    if (nums[m] > target) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+  return l;
+};
