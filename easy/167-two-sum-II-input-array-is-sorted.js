74eb4beb262599025dbeeba932f26ba1e4b9a6b7
Michael Pautov <michael.pautov@gmail.com>
2019-11-11 09:32:00 +0600

    feat(167): solved

---
+++ easy/167-two-sum-II-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let l = 0; h = numbers.length - 1;
+  while (l < h) {
+    const sum = numbers[l] + numbers[h];
+    if (sum === target) return [l + 1, h + 1];
+    if (sum > target) {
+      h--;
+    } else {
+      l++;
+    }
+  }
+  return [-1, -1];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
