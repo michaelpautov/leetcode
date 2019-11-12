3b56c8c908369e81a88383e5145cac3324895c84
Michael Pautov <michael.pautov@gmail.com>
2019-11-12 09:34:04 +0600

    feat(746): solved

---
+++ easy/746-min-cost-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  let f1 = 0, f2 = 0;
+  for(let i = cost.length - 1; i >= 0; i--) {
+    let f0 = cost[i] + Math.min(f1,f2);
+    f2 = f1
+    f1 = f0
+  }
+  return Math.min(f1,f2);
+};
+
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
