d163c216802cca86c22ebf77f7fba27b87abf0f6
Michael Pautov <michael.pautov@gmail.com>
2019-11-22 10:08:15 +0600

    feat(252): solved

---
+++ easy/252-meeting-rooms.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+  intervals.sort((a,b) => {
+    return a[0] - b[0]
+  })
+
+  for(let i = 0; i < intervals.length - 1; i++) {
+    if (intervals[i][1] > intervals[i + 1][0])  {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(canAttendMeetings([[0, 30], [5, 10], [15, 20]]));
+console.log(canAttendMeetings([[7, 10], [2, 4]]));
