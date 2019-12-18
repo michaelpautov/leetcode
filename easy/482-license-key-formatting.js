304aacd38224c221f08c7bb0e70c9923626ffd1f
Michael Pautov <michael.pautov@gmail.com>
2019-12-18 09:16:36 +0600

    feat(482): solved

---
+++ easy/482-license-key-formatting.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+  const raw = S.replace(/-/g, '').toUpperCase();
+  let length = raw.length, chunks = [];
+  while (length > 0) {
+    chunks.push(raw.substring(length - K, length));
+    length -= K;
+  }
+  return chunks.reverse().join('-');
+};
+
+console.log(licenseKeyFormatting("5F3Z-2e-9-w", 4) === "5F3Z-2E9W");
+
+/*
+
+  I should slice string by the end
+  then if latest iterate not equal K
+  I just update latest
+
+*/
