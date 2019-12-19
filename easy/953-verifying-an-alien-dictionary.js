913cc7434fb12942b67064653b40806660f53aa9
Michael Pautov <michael.pautov@gmail.com>
2019-12-19 09:38:14 +0600

    feat(953): solved

--- easy/953-verifying-an-alien-dictionary.js
+++ easy/953-verifying-an-alien-dictionary.js
@@ -4,47 +4,34 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-  if (words.length === 1) return true;
   const hash = {};
-  for(let i = 0; i < order.length; i++) {
-    hash[order[i]] = order.length - i;
+  for(let i = 1; i < order.length; i++) {
+    hash[order[i]] = i;
   }
-  function isSorted(word1, word2) {
-    for(let i = 0; i < word1.length;i++) {
-      const char1 = word1[i];
-      const char2 = word2[i];
-      if (char1 === undefined || char2 === undefined) break;
-      if (char1 === char2) continue;
-      const alien1 = hash[char1];
-      const alien2 = hash[char2];
-      if (alien1 > alien2) return true;
-      if (alien1 < alien2) return false;
+  function isSorted(a, b) {
+    for(let i = 0; i < a.length; i++) {
+      const aV = hash[a[i]], bV = hash[b[i]];
+      if (!aV || !bV) break;
+      if (aV > bV) {
+        return false;
+      } else if (aV < bV) {
+        return true;
+      }
     }
-    return word1.length <= word2.length;
+    return a.length < b.length;
   }
   for(let i = 1; i < words.length; i++) {
-    if(!isSorted(words[i-1], words[i])) {
-      return false;
+    const w1 = words[i-1], w2 = words[i];
+    if (!isSorted(w1, w2)) {
+      return false
     }
   }
   return true;
 };
-/*
-  1. we should add character value to hash by alien alphabetically. Then we should use that hash for compare
-  2. we should create function which will check is word1 are smaller then word2
-    ** function implementation
-    init char1 counter
-    // hd hl
-    1. we should go throw character and check and saving counter of char1
-      if char1 > char2 ? break;
-      if char1 === char2 ? continue
-      if char1 < char2 ? return false
-      if char1 or char2 === null ? break
-    2. if char1.length > char2.length return false
-  3. iterate array use function
-*/
 
 console.log(isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz") === true);
 console.log(isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz") === false);
 console.log(isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz") === false);
 console.log(isAlienSorted(["kuvp", "q"], "ngxlkthsjuoqcpavbfdermiywz") === true);
+console.log(isAlienSorted(["ubg", "kwh"],
+    "qcipyamwvdjtesbghlorufnkzx") === true);
