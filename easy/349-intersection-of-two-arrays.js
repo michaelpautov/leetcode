96f000a25f82715ed56c9731a24abb9d42ec3c26
Michael Pautov <michael.pautov@gmail.com>
2019-12-19 11:09:02 +0600

    feat(349): solved

--- easy/349-intersection-of-two-arrays.js
+++ easy/349-intersection-of-two-arrays.js
@@ -4,19 +4,17 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-  const s1 = new Set(nums1), s2 = new Set(nums2);
-  return s1.size > s2.size ? intersectionSet(s2, s1) : intersectionSet(s1, s2);
-};
-
-function intersectionSet(s1, s2) {
-  const res = [];
-  s1.forEach(v => {
-    if (s2.has(v)) {
-      res.push(v);
+  if (nums1.length < nums2.length) return intersection(nums2, nums1);
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+  let ans = [];
+  for( let val of set2.values()) {
+    if (set1.has(val)) {
+      ans.push(val);
     }
-  });
-  return res;
-}
+  }
+  return ans;
+};
 
 console.log(intersection([1, 2, 2, 1], [2, 2]));
 console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
