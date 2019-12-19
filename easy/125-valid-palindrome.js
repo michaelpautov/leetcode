d7b2419b9c116eb3040001fd029e9753c3f4101a
Michael Pautov <michael.pautov@gmail.com>
2019-12-19 11:09:07 +0600

    feat(125): solved

--- easy/125-valid-palindrome.js
+++ easy/125-valid-palindrome.js
@@ -3,28 +3,32 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  if(s.length === 0 || s.length === 1) {
-    return true;
-  }
+  if (s.length === 0 || s.length === 1) return true;
   let i = 0, j = s.length - 1;
-  while(i <= j) {
-    if (!isAlphaNumeric(s[i])) {
+  while(i < j) {
+    const l = s[i], r = s[j];
+    const lValid = isAlphanumeric(l);
+    if (!lValid) {
       i++;
-    } else if(!isAlphaNumeric(s[j])) {
-      j--;
-    } else {
-      if (s[i].toLowerCase() !== s[j].toLowerCase()) {
-        return false;
-      }
-      i++;
-      j--;
+      continue;
     }
+    const rValid = isAlphanumeric(r);
+    if (!rValid) {
+      j--;
+      continue;
+    }
+    if (l.toLowerCase() !== r.toLowerCase()) {
+      return false;
+    }
+    i++;
+    j--;
   }
   return true;
 };
 
-function isAlphaNumeric(char) {
-  return char.match(/[a-zA-Z]|[0-9]/);
+
+function isAlphanumeric(c) {
+  return !!c.match(/^[a-z0-9]+$/i);
 }
 
 console.log(isPalindrome("A man, a plan, a canal: Panama") === true);
