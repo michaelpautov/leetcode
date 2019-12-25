0b27f7bb81bcf04fda77fba569a26b265691a0ee
Michael Pautov <michael.pautov@gmail.com>
2019-12-25 11:45:07 +0600

    feat(560, 56): repeat

--- middle/56-merge-intervals.js
+++ middle/56-merge-intervals.js
@@ -3,19 +3,22 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-  intervals.sort((a, b) => a[0] - b[0])
+  if (intervals.length === 0) return [];
+  intervals.sort((a,b) => a[0] - b[0])
   let ans = [];
-  for(let i = 0; i < intervals.length; i++) {
-    const [n1, n2] = intervals[i];
-    if (ans.length === 0 || ans[ans.length-1][1] < n1) {
-      ans.push([n1, n2])
+  ans[0] = intervals[0];
+  for(let i = 1;i < intervals.length; i++) {
+    const [x1, x2] = ans[ans.length-1];
+    const [y1, y2] = intervals[i];
+    if (x2 < y1) {
+      ans.push([y1, y2]);
     } else {
-      ans[ans.length-1][1] = Math.max(n2, ans[ans.length-1][1]);
+      ans[ans.length-1][1] = Math.max(x2, y2);
     }
   }
   return ans;
 };
-//
+
 console.log(isArrayEqual(merge([[1, 4], [4, 5]]), [[1,5]]));
 console.log(isArrayEqual(merge([[1,4],[2,3]]), [[1,4]]));
 console.log(isArrayEqual(merge([[1,4],[0,2],[3,5]]), [[0,5]]));
--- middle/560-subarray-sum-equals-k.js
+++ middle/560-subarray-sum-equals-k.js
@@ -4,23 +4,19 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-  let map = new Map(), sum = 0, count = 0;
-  map.set(0, 1)
+  let count = 0, sum = 0, map = new Map();
+  map.set(0, 1);
   for(let i = 0; i < nums.length; i++) {
     sum += nums[i];
-    if (map.has(sum - k)) {
+    if (map.has(sum-k)) {
       count += map.get(sum - k);
     }
-    if (map.has(sum)) {
-      map.set(sum, map.get(sum) + 1)
-    } else {
-      map.set(sum, 1)
-    }
+    map.set(sum, (map.get(sum) || 0) + 1)
   }
   return count;
 };
 
-console.log(subarraySum([-1, 3, -4, 8, -4], 2) === 2);
-console.log(subarraySum([6, -4], 2) === 1);
-console.log(subarraySum([1, 2, 3], 3) === 2);
+// console.log(subarraySum([-1, 3, -4, 8, -4], 2) === 2);
+// console.log(subarraySum([6, -4], 2) === 1);
+// console.log(subarraySum([1, 2, 3], 3) === 2);
 //
