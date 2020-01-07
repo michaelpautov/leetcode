4631ceeeadc103c4c3c4ea92725474b538f7312c
Michael Pautov <michael.pautov@gmail.com>
2020-01-07 13:02:12 +0600

    feat(253): solved

--- middle/253-meeting-rooms-2.js
+++ middle/253-meeting-rooms-2.js
@@ -5,7 +5,7 @@
 var minMeetingRooms = function(intervals) {
   if (intervals.length === 0) return 0;
   if (intervals.length === 1) return 1;
-  let starts = intervals.sort((a,b) => a[0] - b[0]);
+  let starts = intervals.concat([]).sort((a,b) => a[0] - b[0]);
   let ends = intervals.sort((a,b) => a[1] - b[1]);
   let rooms = 0;
   let end = 0;
@@ -22,3 +22,4 @@ var minMeetingRooms = function(intervals) {
 console.log(minMeetingRooms([[0, 30], [5, 10], [15, 20]]) === 2);
 console.log(minMeetingRooms([[7, 10], [2, 4]]) === 1);
 console.log(minMeetingRooms([[0, 5], [6, 7], [5,7], [1,4], [0, 7]]) === 3);
+console.log(minMeetingRooms([[2, 15], [36, 45], [9, 29], [16, 23], [4, 9]]) === 2);
