8b02dd0c93f7932951ea4105193b98aaac308500
Michael Pautov <michael.pautov@gmail.com>
2020-04-14 10:35:29 +0600

    feat(33): solved

---
+++ middle/33.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = start + (end - start)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
+
+
+t = Solution()
+print(t.search([4, 5, 6, 7, 0, 1, 2], 0))
