23d4e2fa98ed9d0baac1df4557f977a001d812a5
Michael Pautov <michael.pautov@gmail.com>
2020-12-24 10:09:14 +0600

    repeat(20): repeated

--- easy/20-valid-parentheses.js
+++ easy/20-valid-parentheses.js
@@ -2,28 +2,6 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-  if (s.length === 1) {
-    return false
-  }
-  const stack = [];
-  for(let i = 0; i < s.length; i++) {
-    const v = s[i];
-    if (v === '[' || v === '(' || v === '{') {
-      stack.push(v)
-    } else {
-      if (stack.pop() !== altBrack(v)) {
-        return false
-      }
-    }
-  }
-  if (stack.length) {
-    return false
-  }
-  return true;
-};
-
-
 var altBrack = function(bracket) {
   switch (bracket) {
     case ')':
@@ -35,8 +13,27 @@ var altBrack = function(bracket) {
   }
 }
 
+var isValid = function(s) {
+  if (s.length === 1) {
+    return false
+  }
+  const stack = []
+  for(let i = 0; i < s.length; i++) {
+      const v = s[i]
+      if (v === '{' || v === '(' || v === '[') {
+        stack.push(v)
+      } else {
+        if (stack.pop() !== altBrack(v)) {
+          return false
+        }
+      }
+  }
+  return !stack.length
+};
 
-// console.log(isValid('()[]{}'));
-// console.log(isValid('([)]'));
+
+
+console.log(isValid('()[]{}'));
+console.log(isValid('([)]'));
 console.log(isValid('(['));
 console.log(isValid(')]'));
